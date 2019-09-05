fbb62f235053fafb050e369f2ac34324a518ce86
DOA <lqw1825830267@gmail.com>
2019-09-05 23:43:52 +0800

    ML programming

--- MachineLearning/Programing Assignment/Week 7/machine-learning-ex6/ex6/processEmail.m
+++ MachineLearning/Programing Assignment/Week 7/machine-learning-ex6/ex6/processEmail.m
@@ -97,7 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
+    len = length(vocabList);
+    for i=1:len
+       if strcmp(str,vocabList{i})
+          word_indices = [word_indices;i]; 
+       end
+    end
 
 
 
