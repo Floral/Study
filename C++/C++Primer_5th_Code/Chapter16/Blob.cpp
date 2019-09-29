8dba8db12988dad20f0b0d5e29ede4ee716e10d3
Floral <mr.qw.liang@gmail.com>
2019-09-29 20:11:42 +0800

    C++ chapter 16 practice

---
+++ C++/C++Primer_5th_Code/Chapter16/Blob.cpp
@@ -0,0 +1,38 @@
+#include"Bolb.h"
+
+template<typename T>
+Blob<T>::Blob(): data(std::make_shared<std::vector<T>>()) {}
+
+template<typename T>
+Blob<T>::Blob(std::initializer_list<T> il):
+        data(std::make_shared<std::vector<T>>(il)) {}
+        
+
+template<typename T>
+void Blob<T>::check(size_type i, const std::string &msg) const
+{
+    if(i>=data->size())
+        throw std::out_of_range(msg);
+}
+
+template<typename T>
+T& Blob<T>::back()
+{
+    check(0, "back on empty Blob");
+    return data->back();
+}
+
+template<typename T>
+T& Blob<T>::operator[](size_type i)
+{
+    check(i, "subscript out of range");
+    return (*data)[i];
+}
+
+template<typename T>
+void Blob<T>::pop_back()
+{
+    check(0, "pop_back on empty Blob");
+    data->pop_back();
+}
+
---
+++ C++/C++Primer_5th_Code/Chapter16/Bolb.h
@@ -0,0 +1,32 @@
+#ifndef BLOB
+#define BLOB
+
+#include<iostream>
+#include<vector>
+#include<memory>
+
+template <typename T> class Blob{
+public:
+    typedef T value_type;
+    typedef typename std::vector<T>::size_type size_type;
+    Blob();
+    Blob(std::initializer_list<T> il);
+
+    size_type size() const { return data->size();}
+    bool empty() const { return data->empty(); }
+
+    void push_back(const T &t) { data->push_back(t); }
+    void push_back(const T &&t) { data->push_back(std::move(t)); }
+    void pop_back();
+
+    T& back();
+    T& operator[](size_type i);
+
+private:
+    std::shared_ptr<std::vector<T>> data;
+
+    void check(size_type i, const std::string &msg) const;
+
+};
+
+#endif
