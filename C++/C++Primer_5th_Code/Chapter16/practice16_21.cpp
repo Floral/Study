8a36ef5e1f0d3bbb44cd0fb0e4fc308ae2689097
Floral <mr.qw.liang@gmail.com>
2019-10-05 00:05:22 +0800

    C++

---
+++ C++/C++Primer_5th_Code/Chapter16/practice16_21.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+class DebugDelete{
+public:
+    DebugDelete(std::ostream &s = std::cerr):os(s) {}
+    template <typename T> void operator()(T *p) const
+    {
+        os<<"deleting unique_ptr"<<std::endl;
+        delete p;
+    }
+private:
+    std::ostream &os;
+};
+
+using namespace std;
+
