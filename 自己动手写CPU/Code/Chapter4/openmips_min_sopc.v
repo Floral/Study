457de0f1d7b2b1b36dd22ef92b3240e26b37f868
DOA <mr.qw.liang@gmail.com>
2020-01-20 16:25:35 +0800

    完成五级流水线以及ori指令

---
+++ 自己动手写CPU/Code/Chapter4/openmips_min_sopc.v
@@ -0,0 +1,34 @@
+
+`include "defines.v"
+
+module openmips_min_sopc(
+
+	input wire	clk,
+	input wire	rst
+	
+);
+
+  //连接指令存储器
+	wire[`InstAddrBus] inst_addr;
+	wire[`InstBus] inst;
+	wire rom_ce;
+ 
+	//实例化处理器opem MIPS
+ 	openmips openmips0(
+		.clk(clk),
+		.rst(rst),
+	
+		.rom_data_i(inst),
+		.rom_addr_o(inst_addr),
+		.rom_ce_o(rom_ce)
+	
+	);
+	
+	inst_rom inst_rom0(
+		.ce(rom_ce)	
+		.addr(inst_addr),
+		.inst(inst),
+	);
+
+
+endmodule
