a6ddc4e15faf14d9c0b6b92c87a5d5acbd291eb9
DOA <qw.liang@outlook.com>
2020-02-27 20:43:04 +0800

    Probing Hash Table

---
+++ 数据结构与算法/数据结构与算法分析（C++）/Code/Chapter5/ProbHashTable.h
@@ -0,0 +1,144 @@
+#ifndef MY_PROBING_HASH_TABLE
+#define MY_PROBING_HASH_TABLE
+
+#include<vector>
+#include<algorithm>
+#include<functional>
+
+template<typename T>
+class HashTable
+{
+public:
+    enum EntryType{ ACTIVE, EMPTY, DELETED};
+
+private:
+    struct HashEntry
+    {
+        T element;
+        EntryType info;
+
+        HashEntry(const T& e = T{}, EntryType i = EMPTY)
+        :element{e}, info{i} {}
+
+        HashEntry(T&& e, EntryType i = EMPTY)
+        :element{std::move(e)}, info{i} {}
+    };
+
+    std::vector<HashEntry> array;   // 链式hash表
+    int currentSize;
+
+    bool isActive(int currentPos) const;
+    int findPos(const T& x) const;
+    void rehash();
+    size_t myhash(const T& x) const;
+
+    
+public:
+    explicit HashTable(int size = 101):currentSize{0}
+    { array.reserve(size); }
+    ~HashTable()
+    { makeEmpty(); }
+
+    bool contains(const T& x) const;
+    void makeEmpty();
+
+    bool insert(const T& x);
+    bool insert(T&& x);
+    bool remove(const T& x);
+};
+
+
+template<typename T>
+size_t HashTable<T>::myhash(const T& x) const
+{
+    static hash<T> hf;
+    return hf(x) % array.size();
+}
+
+template<typename T>
+void HashTable<T>::makeEmpty()
+{
+    currentSize = 0;
+    for(auto& entry: array)
+        entry.info = EMPTY;
+}
+
+template<typename T>
+bool HashTable<T>::contains(const T& x) const
+{
+    return isActive(findPos(x));
+}
+
+template<typename T>
+int HashTable<T>::findPos(const T& x) const //该函数的任务是找到与x相等的元素，即使为deleted也要返回其位置
+{
+    size_t currentPos = myhash(x);
+    int offset = 1;
+
+    while (array[currentPos].info != EMPTY &&
+           array[currentPos].element != x)
+    {
+        currentPos += offset;       //平方消解函数
+        offset += 2;
+
+        if (currentPos>=currentSize)
+            currentPos -= currentSize;
+    }
+    
+    return currentPos;
+}
+
+template<typename T>
+bool HashTable<T>::isActive(int currentPos) const
+{
+    return array[currentPos].info == ACTIVE;
+}
+
+template<typename T>
+bool HashTable<T>::insert(const T& x)
+{
+    int currentPos = findPos(x);
+
+    if (isActive(currentPos))
+        return false;
+
+    array[currentPos].info = ACTIVE;
+    array[currentPos].element = x;
+
+    if (++currentSize > array.size()/2)
+        rehash();
+    
+    return true;
+}
+
+template<typename T>
+bool HashTable<T>::insert(T&& x)
+{
+    int currentPos = findPos(x);
+
+    if (isActive(currentPos))
+        return false;
+
+    array[currentPos].info = ACTIVE;
+    array[currentPos].element = std::move(x);
+
+    if (++currentSize > array.size()/2)
+        rehash();
+    
+    return true;
+}
+
+template<typename T>
+bool HashTable<T>::remove(const T& x)
+{
+    int currentPos = findPos(x);
+
+    if (!isActive(currentPos))
+        return false;
+
+    array[currentPos].info = DELETED;
+
+    return true;
+}
+
+#endif
